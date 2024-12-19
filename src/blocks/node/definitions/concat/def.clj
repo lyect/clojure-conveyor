f76cfd088a17edcd23848da49b244507f99c899a
gudkovst <69453922+gudkovst@users.noreply.github.com>
2024-12-19 23:19:48 +0700

    fix paths

--- src/blocks/node/definitions/concat/def.clj
+++ src/blocks/node/definitions/concat/def.clj
@@ -3,7 +3,7 @@
             [blocks.channel.methods                  :as channel-methods]
             [blocks.channel.types                    :as channel-types]
             [blocks.node.base                        :as node-base]
-            [blocks.node.definitions.base.fields     :as base-node-fields]
+            [blocks.node.definitions.node.fields     :as base-node-fields]
             [blocks.node.definitions.concat.fields   :as concat-node-fields]
             [blocks.node.definitions.node.def        :as base-node-def]
             [blocks.node.methods                     :as node-methods]
