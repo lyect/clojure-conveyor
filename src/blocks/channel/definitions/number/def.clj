e5fe269fe5ce0c57cb1a42d0de300e52befe1154
gudkovst <69453922+gudkovst@users.noreply.github.com>
2024-12-20 20:43:02 +0700

    fix define

--- src/blocks/channel/definitions/number/def.clj
+++ src/blocks/channel/definitions/number/def.clj
@@ -1,7 +1,7 @@
 (ns blocks.channel.definitions.number.def
   (:require [blocks.channel.definitions.channel.def   :as base-channel-def]
             [blocks.channel.definitions.number.fields :as number-channel-fields]
-            [blocks.channel.hierarchy                 :as channel-hierarchy]
+            [blocks.channel.base                      :as channel-base]
             [blocks.channel.properties                :as channel-properties]
             [blocks.channel.types                     :as channel-types]))
 
@@ -9,6 +9,6 @@
 (defn define-number-channel []
   (when-not (channel-types/defined? channel-types/NumberT)
     (base-channel-def/define-base-channel)
-    (alter channel-hierarchy/tree #(assoc % channel-types/NumberT {channel-properties/type-name  channel-types/NumberT
-                                                                   channel-properties/super-name channel-types/ChannelT
-                                                                   channel-properties/fields     number-channel-fields/fields-list}))))
+    (channel-base/define-channel-type channel-types/NumberT
+                                      channel-properties/super-name channel-types/ChannelT
+                                      channel-properties/fields     number-channel-fields/fields-list)))
