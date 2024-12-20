6c21c5ab07638cbbc1d23d1f080e3b02c11ac6d4
gudkovst <69453922+gudkovst@users.noreply.github.com>
2024-12-20 20:48:03 +0700

    fix define

--- src/blocks/node/definitions/node/def.clj
+++ src/blocks/node/definitions/node/def.clj
@@ -5,12 +5,13 @@
             [blocks.node.types                   :as node-types]))
 
 (defn define-base-node []
-  (when-not (node-hierarchy/tree node-types/NodeT)
-    (alter node-hierarchy/tree #(assoc % node-types/NodeT {node-properties/type-name        node-types/NodeT
-                                                           node-properties/super-name       nil
-                                                           node-properties/inputs           nil
-                                                           node-properties/outputs          nil
-                                                           node-properties/function         nil
-                                                           node-properties/ready-validator  nil
-                                                           node-properties/inputs-validator nil
-                                                           node-properties/fields           base-node-fields/fields-list}))))
+  (dosync
+    (when-not (node-hierarchy/tree node-types/NodeT)
+      (alter node-hierarchy/tree #(assoc % node-types/NodeT {node-properties/type-name        node-types/NodeT
+                                                             node-properties/super-name       nil
+                                                             node-properties/inputs           nil
+                                                             node-properties/outputs          nil
+                                                             node-properties/function         nil
+                                                             node-properties/ready-validator  nil
+                                                             node-properties/inputs-validator nil
+                                                             node-properties/fields           base-node-fields/fields-list})))))
