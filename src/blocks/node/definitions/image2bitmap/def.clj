519a189eff07e792e5b8a26859b628671bf65a40
gudkovst <69453922+gudkovst@users.noreply.github.com>
2024-12-20 20:47:35 +0700

    fix define

--- src/blocks/node/definitions/image2bitmap/def.clj
+++ src/blocks/node/definitions/image2bitmap/def.clj
@@ -1,5 +1,7 @@
 (ns blocks.node.definitions.image2bitmap.def
-  (:require [blocks.channel.definitions.image.fields     :as image-channel-fields]
+  (:require [blocks.channel.definitions.image.def :as image-channel-def]
+            [blocks.channel.definitions.bitmap.def :as bitmap-channel-def]
+            [blocks.channel.definitions.image.fields     :as image-channel-fields]
             [blocks.channel.methods                      :as channel-methods]
             [blocks.channel.types                        :as channel-types]
             [blocks.node.base                            :as node-base]
@@ -39,6 +41,8 @@
 (defn define-image2bitmap-node []
   (when-not (node-types/defined? node-types/Image2BitmapT)
     (base-node-def/define-base-node)
+    (image-channel-def/define-image-channel)
+    (bitmap-channel-def/define-bitmap-channel)
     (node-base/define-node-type node-types/Image2BitmapT
       node-properties/inputs           [channel-types/ImageT]
       node-properties/outputs          [channel-types/BitmapT]
