6a8eff923b694400cdc6b884b7f4c50e8ce9ee5b
gudkovst <69453922+gudkovst@users.noreply.github.com>
2024-12-20 20:41:48 +0700

    fix

--- src/blocks/channel/definitions/image/def.clj
+++ src/blocks/channel/definitions/image/def.clj
@@ -1,7 +1,7 @@
 (ns blocks.channel.definitions.image.def
   (:require [blocks.channel.definitions.channel.def  :as base-channel-def]
             [blocks.channel.definitions.image.fields :as image-channel-fields]
-            [blocks.channel.hierarchy                :as channel-hierarchy]
+            [blocks.channel.base                     :as channel-base]
             [blocks.channel.properties               :as channel-properties]
             [blocks.channel.types                    :as channel-types]))
 
@@ -9,6 +9,6 @@
 (defn define-image-channel []
   (when-not (channel-types/defined? channel-types/ImageT)
     (base-channel-def/define-base-channel)
-    (alter channel-hierarchy/tree #(assoc % channel-types/ImageT {channel-properties/type-name  channel-types/ImageT
-                                                                  channel-properties/super-name channel-types/ChannelT
-                                                                  channel-properties/fields     image-channel-fields/fields-list}))))
+    (channel-base/define-channel-type channel-types/ImageT
+                                      channel-properties/super-name channel-types/ChannelT
+                                      channel-properties/fields     image-channel-fields/fields-list)))
