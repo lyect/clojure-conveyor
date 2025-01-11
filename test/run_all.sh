eb1ffc81a2c70fca613c1aaa53f35e95c4215293
lyect <lyect1@gmail.com>
2025-01-11 08:42:35 +0700

    added all the tests

--- test/run_all.sh
+++ test/run_all.sh
@@ -5,8 +5,8 @@ async_dependency="{org.clojure/core.async {:mvn/version \"1.6.673\"}}"
 DEPS="{:deps ${async_dependency}}"
 
 # Correct order of tests
-#clj -Sdeps "${DEPS}" -M $SCRIPTPATH/channel_define.clj
-#clj -Sdeps "${DEPS}" -M $SCRIPTPATH/channel_create.clj
-#clj -Sdeps "${DEPS}" -M $SCRIPTPATH/node_define.clj
-#clj -Sdeps "${DEPS}" -M $SCRIPTPATH/node_create.clj
+clj -Sdeps "${DEPS}" -M $SCRIPTPATH/channel_define.clj
+clj -Sdeps "${DEPS}" -M $SCRIPTPATH/channel_create.clj
+clj -Sdeps "${DEPS}" -M $SCRIPTPATH/node_define.clj
+clj -Sdeps "${DEPS}" -M $SCRIPTPATH/node_create.clj
 clj -Sdeps "${DEPS}" -M $SCRIPTPATH/conveyor_create.clj
